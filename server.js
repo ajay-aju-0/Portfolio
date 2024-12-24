ee38166a78014aa8138e2e293adb7baab9742546
ajayaju <ajaypadmanabhan01@gmail.com>
2024-12-24 14:23:30 +0530

    Portfolio App

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === "production") {
 
 	// react app
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+		res.sendFile(path.resolve(process.cwd(), "client", "dist", "index.html"));
 	});
 }
 
