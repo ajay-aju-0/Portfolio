574df28d5eb244723ea4e27ec8a60b5f206cfeb1
ajayaju <ajaypadmanabhan01@gmail.com>
2024-12-24 14:40:21 +0530

    Portfolio App

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-// const express = require('express');
-import express from 'express';
+const express = require('express');
+// import express from 'express';
 
 const dbConfig = require('./config/dbConfig');
 
