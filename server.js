5be5176b45c698abcc0f361247d3c2353c0bf548
ajayaju <ajaypadmanabhan01@gmail.com>
2024-12-24 14:31:27 +0530

    Portfolio App

--- server.js
+++ server.js
@@ -23,13 +23,14 @@ app.use("/api/portfolio",portfolioRoute);
 // Serve static files
 app.use('/uploads', express.static('uploads'));
 
+const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/client/dist")));
 
 	// react app
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+		res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 	});
 }
 
