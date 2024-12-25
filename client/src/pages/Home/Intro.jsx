a5f807f8b383235bd8a775c0bcde3ee7ececdf2b
ajay-aju-0 <87907442+ajay-aju-0@users.noreply.github.com>
2024-12-25 07:47:37 +0530

    Update Intro.jsx

--- client/src/pages/Home/Intro.jsx
+++ client/src/pages/Home/Intro.jsx
@@ -14,7 +14,6 @@ const Intro = () => {
           responseType: 'blob'
         });
 
-        blob = new Blob([response.data],{ type : "application/pdf" });
         const url = window.URL.createObjectURL(new Blob([response.data],{ type : "application/pdf" }));
         const link = document.createElement('a');
         link.href = url;
