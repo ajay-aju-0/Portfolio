24a068448f9a64293749ede5b5accc986ccd05aa
ajayaju <ajaypadmanabhan01@gmail.com>
2025-12-14 14:35:39 +0530

    redesign: update image source paths to use API_BASE_URL environment variable

--- client/src/pages/Admin/AdminCertificates.jsx
+++ client/src/pages/Admin/AdminCertificates.jsx
@@ -1,186 +1,240 @@
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { hideLoading, setReloadData, showLoading } from '../../redux/rootSlice';
-import axios from 'axios';
-import { Form, message, Modal } from 'antd';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { hideLoading, setReloadData, showLoading } from "../../redux/rootSlice";
+import axios from "axios";
+import { Form, message, Modal } from "antd";
 
 const AdminCertificates = () => {
-    const { portfolioData } = useSelector((state) => state.root);
-    const dispatch = useDispatch();
-    const { certificate } = portfolioData;
-  
-    const [showAddEditModal, setShowAddEditModal] = useState(false);
-    const [selectedItemForEdit, setSelectedItemForEdit] = useState(null);
-    const [type, setType] = useState('add');
+  const { portfolioData } = useSelector((state) => state.root);
+  const dispatch = useDispatch();
+  const { certificate } = portfolioData;
 
-    // console.log(portfolioData);
+  const [showAddEditModal, setShowAddEditModal] = useState(false);
+  const [selectedItemForEdit, setSelectedItemForEdit] = useState(null);
+  const [type, setType] = useState("add");
 
-    const onFinish = async (values) => {
-      try {
-        dispatch(showLoading());
-        let res;
-  
-        if (selectedItemForEdit) {
-          res = await axios.post("/api/portfolio/update-certificate", {
-            ...values,
-            _id: selectedItemForEdit._id,
-          });
-        } else {
-          res = await axios.post("/api/portfolio/add-certificate", values);
-        }
-  
-        if (res.data.success) {
-          message.success(res.data.message);
-          setShowAddEditModal(false);
-          setSelectedItemForEdit(null);
-          dispatch(hideLoading());
-          dispatch(setReloadData(true));
-          setType("");
-        } else {
-          message.error(res.data.message);
-        }
-  
-        dispatch(hideLoading());
-      } catch (error) {
-        message.error(error.message);
-        dispatch(hideLoading());
+  const [thumbnailFile, setThumbnailFile] = useState(null);
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+  const onFinish = async (values) => {
+    try {
+      dispatch(showLoading());
+
+      const formData = new FormData();
+
+      // append text fields
+      Object.keys(values).forEach((key) => {
+        formData.append(key, values[key]);
+      });
+
+      // append thumbnail file
+      if (thumbnailFile) {
+        formData.append("file", thumbnailFile);
       }
-    };
-  
-    const onDelete = async (item) => {
-      try {
-        dispatch(showLoading());
-        const res = await axios.post("/api/portfolio/delete-certificate", {
-          _id: item._id,
+
+      // append id for edit
+      if (selectedItemForEdit) {
+        formData.append("_id", selectedItemForEdit._id);
+      }
+
+      let res;
+      if (selectedItemForEdit) {
+        res = await axios.post("/api/portfolio/update-certificate", formData, {
+          headers: { "Content-Type": "multipart/form-data" },
         });
-        dispatch(hideLoading());
-  
-        if (res.data.success) {
-          message.success(res.data.message);
-          dispatch(hideLoading());
-          dispatch(setReloadData(true));
-        } else {
-          message.error(res.data.message);
-        }
-      } catch (error) {
-        dispatch(hideLoading());
-        message.error(error.message);
+      } else {
+        res = await axios.post("/api/portfolio/add-certificate", formData, {
+          headers: { "Content-Type": "multipart/form-data" },
+        });
       }
-    };
-  
-    return (
-      <div>
-        <div className="flex justify-end p-2">
-          <button
-            className="bg-green-900 px-5 py-2 text-white font-semibold"
-            onClick={() => {
-              setType('add');
-              setSelectedItemForEdit(null);
-              setShowAddEditModal(true);
-            }}
+
+      if (res.data.success) {
+        message.success(res.data.message);
+        setShowAddEditModal(false);
+        setSelectedItemForEdit(null);
+        dispatch(hideLoading());
+        dispatch(setReloadData(true));
+        setType("");
+      } else {
+        message.error(res.data.message);
+      }
+
+      dispatch(hideLoading());
+    } catch (error) {
+      message.error(error.message);
+      dispatch(hideLoading());
+    }
+  };
+
+  const onDelete = async (item) => {
+    try {
+      dispatch(showLoading());
+      const res = await axios.post("/api/portfolio/delete-certificate", {
+        _id: item._id,
+      });
+      dispatch(hideLoading());
+
+      if (res.data.success) {
+        message.success(res.data.message);
+        dispatch(hideLoading());
+        dispatch(setReloadData(true));
+      } else {
+        message.error(res.data.message);
+      }
+    } catch (error) {
+      dispatch(hideLoading());
+      message.error(error.message);
+    }
+  };
+
+  return (
+    <div>
+      <div className="flex justify-end p-2">
+        <button
+          className="bg-green-900 px-5 py-2 text-white font-semibold"
+          onClick={() => {
+            setType("add");
+            setSelectedItemForEdit(null);
+            setShowAddEditModal(true);
+          }}
+        >
+          Add Certificate
+        </button>
+      </div>
+      <div className="grid grid-cols-3 gap-5 sm:grid-cols-1">
+        {certificate.map((certificate) => (
+          <div
+            className="shadow border p-5 border-gray-400 flex flex-col"
+            key={certificate._id}
           >
-            Add Certificate
-          </button>
-        </div>
-        <div className="grid grid-cols-3 gap-5 sm:grid-cols-1">
-          {certificate.map((certificate) => (
-            <div
-              className="shadow border p-5 border-gray-400 flex flex-col"
-              key={certificate._id}
-            >
-              <h1 className="text-primary text-xl font-bold">
-                {certificate.title}
-              </h1>
-              <h1>{certificate.organisation}</h1>
-              <hr />
-              <h1>{certificate.description}</h1>
-              <h1>Certificate :&nbsp;&nbsp;&nbsp;
-              <a href={certificate.link} target='_blank' className="text-blue-600 mx-1 inline-flex gap-1 text-xl">
-              <svg
-                    fill="currentColor"
-                    viewBox="0 0 16 16"
-                    height="1em"
-                    width="1em"
-                    
-                    >
-                    <path
-                        fillRule="evenodd"
-                        d="M8.636 3.5a.5.5 0 00-.5-.5H1.5A1.5 1.5 0 000 4.5v10A1.5 1.5 0 001.5 16h10a1.5 1.5 0 001.5-1.5V7.864a.5.5 0 00-1 0V14.5a.5.5 0 01-.5.5h-10a.5.5 0 01-.5-.5v-10a.5.5 0 01.5-.5h6.636a.5.5 0 00.5-.5z"
-                    />
-                    <path
-                        fillRule="evenodd"
-                        d="M16 .5a.5.5 0 00-.5-.5h-5a.5.5 0 000 1h3.793L6.146 9.146a.5.5 0 10.708.708L15 1.707V5.5a.5.5 0 001 0v-5z"
-                    />
+            <h1 className="text-primary text-xl font-bold">
+              {certificate.title}
+            </h1>
+            <h1>{certificate.organisation}</h1>
+            <hr />
+            <h1>{certificate.description}</h1>
+            <h1>
+              Certificate :&nbsp;&nbsp;&nbsp;
+              <a
+                href={certificate.link}
+                target="_blank"
+                className="text-blue-600 mx-1 inline-flex gap-1 text-xl"
+              >
+                <svg
+                  fill="currentColor"
+                  viewBox="0 0 16 16"
+                  height="1em"
+                  width="1em"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8.636 3.5a.5.5 0 00-.5-.5H1.5A1.5 1.5 0 000 4.5v10A1.5 1.5 0 001.5 16h10a1.5 1.5 0 001.5-1.5V7.864a.5.5 0 00-1 0V14.5a.5.5 0 01-.5.5h-10a.5.5 0 01-.5-.5v-10a.5.5 0 01.5-.5h6.636a.5.5 0 00.5-.5z"
+                  />
+                  <path
+                    fillRule="evenodd"
+                    d="M16 .5a.5.5 0 00-.5-.5h-5a.5.5 0 000 1h3.793L6.146 9.146a.5.5 0 10.708.708L15 1.707V5.5a.5.5 0 001 0v-5z"
+                  />
                 </svg>
-                </a>
-                </h1>
-              <div className="flex justify-end gap-5 mt-5">
-                <button
-                  className="bg-red-700 text-white px-5 py-2 md"
-                  onClick={() => onDelete(certificate)}
-                >
-                  Delete
-                </button>
-                <button
-                  className="bg-green-900 text-white px-5 py-2 md"
-                  onClick={() => {
-                    setType("edit");
-                    setSelectedItemForEdit(certificate);
-                    setShowAddEditModal(true);
-                  }}
-                >
-                  Edit
-                </button>
-              </div>
+              </a>
+            </h1>
+            <div className="flex justify-end gap-5 mt-5">
+              <button
+                className="bg-red-700 text-white px-5 py-2 md"
+                onClick={() => onDelete(certificate)}
+              >
+                Delete
+              </button>
+              <button
+                className="bg-green-900 text-white px-5 py-2 md"
+                onClick={() => {
+                  setType("edit");
+                  setSelectedItemForEdit(certificate);
+                  setShowAddEditModal(true);
+                }}
+              >
+                Edit
+              </button>
             </div>
-          ))}
-        </div>
-        { (type === "add" || selectedItemForEdit) && <Modal
+          </div>
+        ))}
+      </div>
+      {(type === "add" || selectedItemForEdit) && (
+        <Modal
           open={showAddEditModal}
           title={selectedItemForEdit ? "Edit Certificate" : "Add Certificate"}
           footer={null}
           onCancel={() => {
-              setShowAddEditModal(false);
-              setSelectedItemForEdit(null);
-              setType("");
-          }}>
+            setShowAddEditModal(false);
+            setSelectedItemForEdit(null);
+            setType("");
+          }}
+        >
           <Form
             layout="vertical"
             onFinish={onFinish}
             initialValues={{
-                ...selectedItemForEdit
+              ...selectedItemForEdit,
             }}
           >
             <Form.Item name="title" label="Title">
               <input className="modal-input" type="text" placeholder="Title" />
             </Form.Item>
             <Form.Item name="organisation" label="Organisation">
-              <input className="modal-input" type="text" placeholder="Organisation" />
+              <input
+                className="modal-input"
+                type="text"
+                placeholder="Organisation"
+              />
             </Form.Item>
             <Form.Item name="description" label="Description">
               <textarea className="modal-input" placeholder="Description" />
             </Form.Item>
             <Form.Item name="link" label="Link">
-              <input className="modal-input" type="text" placeholder="Technologies" />
+              <input
+                className="modal-input"
+                type="text"
+                placeholder="Technologies"
+              />
+            </Form.Item>
+            <Form.Item label="Certificate Thumbnail">
+              {selectedItemForEdit?.thumbnail && (
+                <img
+                  src={`${API_BASE_URL}/${selectedItemForEdit.thumbnail}`}
+                  alt="thumbnail"
+                  className="w-1/4 h-20 object-cover mb-3 rounded"
+                />
+              )}
+              <input
+                type="file"
+                accept="image/*"
+                className="modal-input"
+                onChange={(e) => setThumbnailFile(e.target.files[0])}
+              />
             </Form.Item>
             <div className="flex justify-end">
-              <button type="button"
+              <button
+                type="button"
                 className="border-primary text-primary px-5 py-2"
                 onClick={() => {
                   setShowAddEditModal(false);
                   setSelectedItemForEdit(null);
                   setType("");
-              }}>
+                }}
+              >
                 Cancel
               </button>
-              <button type="submit" className="bg-green-900 text-white px-5 py-2">
+              <button
+                type="submit"
+                className="bg-green-900 text-white px-5 py-2"
+              >
                 {selectedItemForEdit ? "Update" : "Add"}
               </button>
             </div>
           </Form>
-        </Modal>}
-      </div>
-    );
-}
+        </Modal>
+      )}
+    </div>
+  );
+};
 
-export default AdminCertificates
+export default AdminCertificates;
--- client/src/pages/Home/Certificates.jsx
+++ client/src/pages/Home/Certificates.jsx
@@ -1,17 +1,34 @@
-import React, { useState } from 'react'
-import SectionTitle from '../../components/SectionTitle'
-import { useSelector } from 'react-redux';
+import React, { useState } from "react";
+import SectionTitle from "../../components/SectionTitle";
+import { useSelector } from "react-redux";
+
+import { useRef } from "react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 const Certificates = () => {
-    const [selectedItemIndex, setSelectedItemIndex] = useState(0)
-    
-    const { portfolioData } = useSelector((state) => state.root);
-    const { certificate } = portfolioData; 
-      
+  const [selectedItemIndex, setSelectedItemIndex] = useState(0);
+
+  const { portfolioData } = useSelector((state) => state.root);
+  const { certificate } = portfolioData;
+
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+  const rowRef = useRef(null);
+
+  function scroll(dir = "right") {
+    const el = rowRef.current;
+    if (!el) return;
+    const amount = el.clientWidth * 0.75; // scroll by 75% of visible area
+    el.scrollBy({
+      left: dir === "right" ? amount : -amount,
+      behavior: "smooth",
+    });
+  }
+
   return (
     <>
-        <SectionTitle title="Certificates" />
-        <div className="flex py-10 gap-20 sm:flex-col">
+      <SectionTitle title="Certificates" />
+      {/* <div className="flex py-10 gap-20 sm:flex-col">
             <div className='flex flex-col gap-10 border-l-2 border-[#135e4c82] w-2/3 sm:flex-row sm:overflow-x-scroll sm:w-full'>
                 {certificate.map((certificate, index) => (
                     <div className='p-1 cursor-pointer' onClick={() => {setSelectedItemIndex(index);}} key={index}>
@@ -42,9 +59,188 @@ const Certificates = () => {
                 </a>
                 
             </div>
-        </div>
-    </>
-  )
-}
+        </div> */}
 
-export default Certificates
+      <section className="w-full py-12 bg-gradient-to-b from-[#07162a] via-[#061a2b] to-[#041221] text-white">
+              <div className="max-w-6xl mx-auto px-4">
+                <div className="flex items-center justify-between mb-6">
+                  {/* <h2 className="text-3xl md:text-4xl font-extrabold">Projects</h2> */}
+                  <p className="hidden md:block text-[#9fc0d6]">
+                    A few highlights — hover to see details.
+                  </p>
+                </div>
+      
+                {/* Desktop Arrows */}
+                <div className="relative">
+                  <button
+                    onClick={() => scroll("left")}
+                    aria-label="Scroll left"
+                    className="hidden md:flex items-center justify-center absolute -left-6 top-1/2 -translate-y-1/2 z-20 w-12 h-12 rounded-full bg-[#0c2431] border border-[#15323f] shadow-md hover:translate-x-[-2px]"
+                  >
+                    <ChevronLeft size={22} />
+                  </button>
+      
+                  <button
+                    onClick={() => scroll("right")}
+                    aria-label="Scroll right"
+                    className="hidden md:flex items-center justify-center absolute -right-6 top-1/2 -translate-y-1/2 z-20 w-12 h-12 rounded-full bg-[#0c2431] border border-[#15323f] shadow-md hover:translate-x-[2px]"
+                  >
+                    <ChevronRight size={22} />
+                  </button>
+      
+                  {/* Scrollable Row */}
+                  <div
+                    ref={rowRef}
+                    className="flex gap-6 overflow-x-auto scroll-smooth snap-x snap-mandatory py-4 no-scrollbar"
+                  >
+                    {certificate.map((c,i) => (
+                      <div
+                        key={i}
+                        className="snap-start min-w-[260px] md:min-w-[320px] bg-[#071827] rounded-2xl border border-[#15303a] overflow-hidden shadow-lg relative group"
+                      >
+                        {/* Image */}
+                        <div className="h-44 md:h-48 w-full overflow-hidden">
+                          <img
+                            src={`${API_BASE_URL}/${c.thumbnail}`}
+                            alt={c.thumbnail}
+                            className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105"
+                          />
+                        </div>
+      
+                        {/* Content */}
+                        <div className="p-4">
+                          <div className="flex items-center gap-3">
+                            <div>
+                              <h3 className="text-lg font-semibold">{c.title}</h3>
+                              <p className="text-sm text-[#9fc0d6]">{c.short}</p>
+                            </div>
+                          </div>
+                        </div>
+      
+                        {/* Hover overlay (reveals on hover) */}
+                        <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-end p-4">
+                          <div className="backdrop-blur-sm rounded-md p-3 bg-black/30">
+                            <p className="text-sm text-[#d6eefb]">{c.description}</p>
+                            <div className="mt-3 flex gap-3">
+                              {/* <a href="#" className="text-xs font-medium px-3 py-1 bg-[#0f2b3a] rounded-md border border-[#15414d]">View</a> */}
+                              <a
+                                href={`${certificate[selectedItemIndex].link}`}
+                                target="_blank"
+                                className="text-xs font-medium px-3 py-1 bg-[#0f2b3a] rounded-md border border-[#15414d]"
+                              >
+                                Link
+                              </a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+      
+                  {/* Mobile overlay arrows (small) */}
+                  <div className="md:hidden absolute inset-y-0 left-2 flex items-center">
+                    <button
+                      onClick={() => scroll("left")}
+                      className="p-2 rounded-full bg-black/40 backdrop-blur border border-[#12323b]"
+                    >
+                      <svg
+                        className="w-5 h-5 text-white"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M15 19l-7-7 7-7"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                  <div className="md:hidden absolute inset-y-0 right-2 flex items-center">
+                    <button
+                      onClick={() => scroll("right")}
+                      className="p-2 rounded-full bg-black/40 backdrop-blur border border-[#12323b]"
+                    >
+                      <svg
+                        className="w-5 h-5 text-white"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M9 5l7 7-7 7"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+      
+                {/* Desktop grid fallback (3 columns) */}
+                <div className="hidden md:grid md:grid-cols-3 md:gap-6 mt-8">
+                  {certificate.map((c,i) => (
+                    <div
+                      key={i}
+                      className="rounded-2xl overflow-hidden border border-[#15303a] bg-[#071827] shadow-md"
+                    >
+                      <img
+                        src={`${API_BASE_URL}/${c.thumbnail}`}
+                        alt={c.thumbnail}
+                        className="w-full h-44 object-cover"
+                      />
+                      <div className="p-4">
+                        <div className="flex items-center gap-3">
+                          <div
+                            style={{ background: c.color }}
+                            className="w-10 h-10 rounded-full flex items-center justify-center shrink-0"
+                          >
+                            <svg
+                              className="w-5 h-5 text-white"
+                              viewBox="0 0 24 24"
+                              fill="none"
+                              stroke="currentColor"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M12 2v20M2 12h20"
+                              />
+                            </svg>
+                          </div>
+                          <div>
+                            <h3 className="text-lg font-semibold">{c.title}</h3>
+                            <p className="text-sm text-[#9fc0d6]">{c.short}</p>
+                          </div>
+                        </div>
+                        
+                        <p className="mt-3 text-sm text-[#cfeaf9]">{c.desc}</p>
+                        <div className="mt-4 flex gap-3">
+                          <a
+                            href="#"
+                            className="text-xs font-medium px-3 py-1 bg-[#0f2b3a] rounded-md border border-[#15414d]"
+                          >
+                            View
+                          </a>
+                          <a
+                            href="#"
+                            className="text-xs font-medium px-3 py-1 bg-[#0f2b3a] rounded-md border border-[#15414d]"
+                          >
+                            Repo
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </section>
+    </>
+  );
+};
+
+export default Certificates;
