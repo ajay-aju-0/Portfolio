98e0a3acbcafe27c52db54047e2c599068ce3973
ajayaju <ajaypadmanabhan01@gmail.com>
2025-12-20 11:23:31 +0530

    responsive

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
-import Header from '../../components/Header'
-import Intro from './Intro'
-import About from './About'
-import Experiences from './Experiences'
-import Projects from './Projects'
-import Certificates from './Certificates'
-import Contact from './Contact'
-import Footer from '../../components/Footer'
-import LeftSider from './LeftSider'
-import { useSelector } from 'react-redux'
+import React from "react";
+import Header from "../../components/Header";
+import Intro from "./Intro";
+import About from "./About";
+import Experiences from "./Experiences";
+import Projects from "./Projects";
+import Certificates from "./Certificates";
+import Contact from "./Contact";
+import Footer from "../../components/Footer";
+import LeftSider from "./LeftSider";
+import { useSelector } from "react-redux";
 
 const Home = () => {
-    const { loading, portfolioData } = useSelector((state) => state.root)
+  const { loading, portfolioData } = useSelector((state) => state.root);
 
   return (
     <>
-    <Header />
-    {portfolioData && 
+      <Header />
+      {portfolioData && (
         // <div className='bg-primary px-40 sm:px-5'>
-        <div className="bg-primary px-40 lg:px-20 md:px-10 sm:px-5">
+        <div className="bg-primary flex justify-center">
+          <div className="w-full max-w-6xl px-10 md:px-6 sm:px-4">
             <Intro />
             <About />
             <Experiences />
@@ -27,10 +28,11 @@ const Home = () => {
             <Contact />
             <Footer />
             <LeftSider />
-        </div> 
-    }
+          </div>
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
